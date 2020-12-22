70a64e28e55ebf0c83593a31c72854c3124821ef
Anisa89 <anisa.bakhteeva1989@gmail.com>
2020-12-22 21:47:32 +0100

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,35 +1,25 @@
-/*Напиши скрипт, который, для объекта user, последовательно:
-
-добавляет поле mood со значением 'happy'
-заменяет значение hobby на 'skydiving'
-заменяет значение premium на false
-выводит содержимое объекта user в формате ключ: значение используя Object.keys() и for...of*/
-
-/*const user = {
-  name: "Mango",
-  age: 20,
-  hobby: "html",
-  premium: true,
+const account = {
+  owner: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["order-1", "order-2", "order-3"],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
 };
-console.log(user);
 
-user.mood = "happy";
-console.log(user);
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-user.hobby = "Skydiving";
-console.log(user);
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
 
-user.premium = false;
-console.log(user);
-
-const keys = Object.keys(user);
-console.log(keys);
-
-/*for (const key of keys) {
-  console.log(key);
-  console.log(user[key]);
-}
-*/
-/*for (const keys of Object.keys(user)) {
-  console.log(`${keys}: ${user[keys]}`);
-}*/
+account.addOrder(5000, "order-4");
+console.log(account.balance); // 19000
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
