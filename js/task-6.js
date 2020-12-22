c247d2f917193733f9a05c1b81e91d21cabf946f
Anisa89 <anisa.bakhteeva1989@gmail.com>
2020-12-22 21:07:14 +0100

    new rep

---
+++ js/task-6.js
@@ -0,0 +1,20 @@
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = (allProdcuts, productName) => {
+  let totalPrice = 0;
+  for (const nameProduct of allProdcuts) {
+    if (nameProduct.name === productName) {
+      totalPrice = nameProduct.price * nameProduct.quantity;
+    }
+  }
+  return totalPrice;
+};
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
