6444c81e3f78a3ae62328c86df10b8f01529c373
Anisa89 <anisa.bakhteeva1989@gmail.com>
2020-12-22 21:17:10 +0100

    new rep

---
+++ js/task-1.js
@@ -0,0 +1,35 @@
+/*Напиши скрипт, который, для объекта user, последовательно:
+
+добавляет поле mood со значением 'happy'
+заменяет значение hobby на 'skydiving'
+заменяет значение premium на false
+выводит содержимое объекта user в формате ключ: значение используя Object.keys() и for...of*/
+
+/*const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+};
+console.log(user);
+
+user.mood = "happy";
+console.log(user);
+
+user.hobby = "Skydiving";
+console.log(user);
+
+user.premium = false;
+console.log(user);
+
+const keys = Object.keys(user);
+console.log(keys);
+
+/*for (const key of keys) {
+  console.log(key);
+  console.log(user[key]);
+}
+*/
+/*for (const keys of Object.keys(user)) {
+  console.log(`${keys}: ${user[keys]}`);
+}*/
